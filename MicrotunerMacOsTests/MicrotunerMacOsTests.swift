05857128f90d080a1e986ff6cd61037e7069d2bd
Che8Sounds <che8sounds@gmail.com>
2025-08-20 00:30:38 +0200

    Initial Commit

---
+++ MicrotunerMacOsTests/MicrotunerMacOsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MicrotunerMacOsTests.swift
+//  MicrotunerMacOsTests
+//
+//  Created by Ghifar on 20.08.25.
+//
+
+import Testing
+@testable import MicrotunerMacOs
+
+struct MicrotunerMacOsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
